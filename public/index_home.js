f5051f13be7b9c5f4aabee3e9a3ec5b11a5607f7
vananhm-1205 <majanhqn@gmail.com>
2019-10-23 11:25:25 +0700

    make it better

--- public/index_home.js
+++ public/index_home.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000/");
+const socket = io("https://anhblog.herokuapp.com");
 
 socket.on("server-send-list-user-information", (data) => {
     $("#list").html("");
